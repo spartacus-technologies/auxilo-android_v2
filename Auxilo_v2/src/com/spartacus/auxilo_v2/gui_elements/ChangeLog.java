91af3376118453d416060555abc2b1a2e50680f5
Eetu <eetu.prehti@gmail..com>
2014-10-19 22:23:18 +0300

    Changelog.

---
+++ Auxilo_v2/src/com/spartacus/auxilo_v2/gui_elements/ChangeLog.java
@@ -0,0 +1,70 @@
+package com.spartacus.auxilo_v2.gui_elements;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import com.spartacus.auxilo_v2.R;
+
+import android.app.Activity;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class ChangeLog extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_change_log);
+		
+		//Set changelog text:
+		AssetManager am = this.getAssets();
+		
+		InputStream is = null;
+		
+		try {
+			is = am.open("changelog.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		BufferedReader r = new BufferedReader(new InputStreamReader(is));
+		StringBuilder total = new StringBuilder();
+		String line;
+		try {
+			while ((line = r.readLine()) != null) {
+			    total.append(line);
+			    total.append("\n");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		line = total.toString();
+		((TextView)findViewById(R.id.changelogText)).setText(line);
+	}
+
+	@Override 
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.change_log, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
--- Auxilo_v2/src/com/spartacus/auxilo_v2/gui_elements/FragmentSettings.java
+++ Auxilo_v2/src/com/spartacus/auxilo_v2/gui_elements/FragmentSettings.java
@@ -1,17 +1,20 @@
 package com.spartacus.auxilo_v2.gui_elements;
 
+
 import com.spartacus.auxilo_v2.R;
 import com.spartacus.auxilo_v2.gui_elements.FragmentSettings;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.webkit.WebView.FindListener;
 import android.widget.TextView;
 
-public class FragmentSettings extends Fragment {
+public class FragmentSettings extends Fragment implements OnClickListener{
 
 	View rootView_ = null;
 	
@@ -30,6 +33,7 @@ public class FragmentSettings extends Fragment {
 		
 		args.putInt(ARG_SECTION_NUMBER, sectionNumber);
 		fragment.setArguments(args);
+		
 		return fragment;
 	}
 
@@ -47,7 +51,28 @@ public class FragmentSettings extends Fragment {
 		//Default values:
 		//((TextView)rootView_.findViewById(R.id.textView1)).setText("Fragment " + ARG_SECTION_NUMBER);
 		
+		//Listeners:
+		rootView_.findViewById(R.id.button_change_log).setOnClickListener(this);
+		
 		return rootView;
+	}
+
+	@Override
+	public void onClick(View v) {
+
+		switch (v.getId()) {
+		
+		case R.id.button_change_log:
+			
+			Intent changelog = new Intent(getActivity(), ChangeLog.class);
+			startActivity(changelog);
+			
+			break;
+
+		default:
+			break;
+		}
+		
 	}
 }
 
